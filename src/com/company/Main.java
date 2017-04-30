c2b9626cc03427f4bf134386f2dd0498159b3f42
tabatadzem <tabatadzem@hotmail.com>
2017-04-30 23:27:32 +0200

    Converting radians to degrees and printing tables using f format

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -147,27 +147,39 @@ public class Main {
 //            else {
 //                System.out.println(ch + " is an invalid input");}
 
-        String lottery = "" + (int)(Math.random() * 10)
-         + (int)(Math.random() * 10);
-        Scanner input = new Scanner(System.in);
-        System.out.print("Enter your lottery pick (two digits): ");
-        String guess = input.nextLine();
-        char lotteryDigit1 = lottery.charAt(0);
-        char lotteryDigit2 = lottery.charAt(1);
-        char guessDigit1 = guess.charAt(0);
-        char guessDigit2 = guess.charAt(1);
-        System.out.println("The lottery number is " + lottery);
-        if (guess.equals(lottery))
-            System.out.println("Exact match: you win $10,000");
-        else if (guessDigit2 == lotteryDigit1
-        && guessDigit1 == lotteryDigit2)
-        System.out.println("Match all digits: you win $3,000");
-        else if (guessDigit1 == lotteryDigit1
-                || guessDigit1 == lotteryDigit2
-                || guessDigit2 == lotteryDigit1
-                || guessDigit2 == lotteryDigit2)
-         System.out.println("Match one digit: you win $1,000");
-        else  System.out.println("Sorry, no match");
+//        String lottery = "" + (int)(Math.random() * 10)
+//         + (int)(Math.random() * 10);
+//        Scanner input = new Scanner(System.in);
+//        System.out.print("Enter your lottery pick (two digits): ");
+//        String guess = input.nextLine();
+//        char lotteryDigit1 = lottery.charAt(0);
+//        char lotteryDigit2 = lottery.charAt(1);
+//        char guessDigit1 = guess.charAt(0);
+//        char guessDigit2 = guess.charAt(1);
+//        System.out.println("The lottery number is " + lottery);
+//        if (guess.equals(lottery))
+//            System.out.println("Exact match: you win $10,000");
+//        else if (guessDigit2 == lotteryDigit1
+//        && guessDigit1 == lotteryDigit2)
+//        System.out.println("Match all digits: you win $3,000");
+//        else if (guessDigit1 == lotteryDigit1
+//                || guessDigit1 == lotteryDigit2
+//                || guessDigit2 == lotteryDigit1
+//                || guessDigit2 == lotteryDigit2)
+//         System.out.println("Match one digit: you win $1,000");
+//        else  System.out.println("Sorry, no match");
+
+        System.out.printf("%-10s%-10s%-10s%-10s%-10s\n", "Degrees",
+               "Radians", "Sine", "Cosine", "Tangent");
+        int degrees = 30;
+        double radians = Math.toRadians(degrees);
+            System.out.printf("%-10d%-10.4f%-10.4f%-10.4f%-10.4f\n", degrees,
+                radians, Math.sin(radians), Math.cos(radians), Math.tan(radians));
+        degrees = 60;
+        radians = Math.toRadians(degrees);
+        System.out.printf("%-10d%-10.4f%-10.4f%-10.4f%-10.4f\n", degrees,
+                 radians, Math.sin(radians), Math.cos(radians),
+                 Math.tan(radians));
 
     }
 }
