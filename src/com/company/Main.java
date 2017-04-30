208c3f0c368aced04863531c8cbd70a440619a13
tabatadzem <tabatadzem@hotmail.com>
2017-04-30 19:58:09 +0200

    exercise "guess birthday"

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -30,35 +30,107 @@ public class Main {
 //                    Math.round(A * 100) / 100.0 + " " + Math.round(B * 100) / 100.0 + " " +
 //             Math.round(C * 100) / 100.0);
 
+//
+//         int card = (int) (Math.random()*53);
+//         int suit =  (card /13);
+//            int face =  (card /4);
+//
+//            char faceValue ='0';
+//          String suitvalue = "";
+//          if (face > 0 && face < 9) {
+//              faceValue = (char) (49 + face);
+//          }
+//
+//          switch(face){
+//             case 0:  faceValue = 'A'; break;
+//             case 9: faceValue = '1'; break;
+//             case 10: faceValue = 'J'; break;
+//             case 11: faceValue = 'Q'; break;
+//             case 12: faceValue = 'k'; break;
+//         }
+//         switch (suit){
+//              case 0: suitvalue = "Spades"; break;
+//              case 1: suitvalue = "diamond"; break;
+//             case 2: suitvalue = "club"; break;
+//             case 3: suitvalue= "heart"; break;
+//          }
+//         System.out.println("your card is " + faceValue +" of " + suitvalue);
+//
+//
+//      int A = 'C';
+//      System.out.print(A);
+//
+//        char ch = 'a';
+//        System.out.println("isDigit('a') is " + Character.isDigit('a'));
+//        System.out.println("isLetter('a') is " + Character.isLetter('a'));
+//        System.out.println("isLowerCase('a') is "
+//                + Character.isLowerCase('a'));
+//        System.out.println("isUpperCase('a') is "
+//                + Character.isUpperCase('a'));
+//        System.out.println("toLowerCase('T') is "
+//                + Character.toLowerCase('T'));
+//        System.out.println("toUpperCase('q') is "
+//                + Character.toUpperCase('q'));
 
-         int card = (int) (Math.random()*53);
-         int suit =  (card /13);
-            int face =  (card /4);
 
-            char faceValue ='0';
-          String suitvalue = "";
-          if (face > 0 && face < 9) {
-              faceValue = (char) (49 + face);
-          }
+        String set1 = " 1 3 5 7\n"+
+                      " 9 11 13 15\n" +
+                      "17 19 21 23\n" +
+                      "25 27 29 31";
 
-          switch(face){
-             case 0:  faceValue = 'A'; break;
-             case 9: faceValue = '1'; break;
-             case 10: faceValue = 'J'; break;
-             case 11: faceValue = 'Q'; break;
-             case 12: faceValue = 'k'; break;
-         }
-         switch (suit){
-              case 0: suitvalue = "Spades"; break;
-              case 1: suitvalue = "diamond"; break;
-             case 2: suitvalue = "club"; break;
-             case 3: suitvalue= "heart"; break;
-          }
-         System.out.println("your card is " + faceValue +" of " + suitvalue);
+        String set2 = " 2 3 6 7\n" +
+                      "10 11 14 15\n" +
+                      "18 19 22 23\n" +
+                      "26 27 30 31";
+
+        String set3 = " 4 5 6 7\n" +
+                      "12 13 14 15\n" +
+                      "20 21 22 23\n" +
+                      "28 29 30 31";
+
+        String set4 = " 8 9 10 11\n" +
+                      "12 13 14 15\n" +
+                      "24 25 26 27\n" +
+                      "28 29 30 31";
+
+        String set5 = "16 17 18 19\n" +
+                      "20 21 22 23\n" +
+                      "24 25 26 27\n" +
+                      "28 29 30 31";
+        int day = 0;
+        Scanner input = new Scanner(System.in);
+        System.out.print(set1);
+        System.out.print("\nEnter 0 for No and 1 for Yes: ");
+        int answer = input.nextInt();
+        if (answer == 1){
+            day +=1;}
+        System.out.print("\nIs your birthday in Set2?\n");
+        System.out.print(set2);
+        System.out.print("\nEnter 0 for No and 1 for Yes: ");
+        answer = input.nextInt();
+        if (answer == 1){
+            day +=2;}
+        System.out.print("\nIs your birthday in Set3?\n");
+        System.out.print(set3);
+        System.out.print("\nEnter 0 for No and 1 for Yes: ");
+        answer = input.nextInt();
+        if (answer == 1){
+            day +=4;}
+        System.out.print("\nIs your birthday in Set4?\n");
+        System.out.print(set4);
+        System.out.print("\nEnter 0 for No and 1 for Yes: ");
+        answer = input.nextInt();
+        if (answer == 1){
+            day +=8;}
+        System.out.print("\nIs your birthday in Set5?\n");
+        System.out.print(set5);
+        System.out.print("\nEnter 0 for No and 1 for Yes: ");
+        answer = input.nextInt();
+        if (answer == 1){
+            day +=16;}
+        System.out.println("\nYour birthday is " + day + "!");
 
 
-      int A = 'C';
-      System.out.print(A);
 
     }
 }
