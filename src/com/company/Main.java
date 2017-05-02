a25b286efe468f4dfde8a8b2f9b421700eea4b89
tabatadzem <tabatadzem@hotmail.com>
2017-05-02 09:38:43 +0200

    exercise 4..11

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -314,67 +314,93 @@ public class Main {
 //      System.out.println(i);
 
 
-        String set1 = " 1 3 5 7\n"+
-              " 9 11 13 15\n" +
-              "17 19 21 23\n" +
-              "25 27 29 31";
+//        String set1 = " 1 3 5 7\n"+
+//              " 9 11 13 15\n" +
+//              "17 19 21 23\n" +
+//              "25 27 29 31";
+//
+//        String set2 = " 2 3 6 7\n" +
+//             "10 11 14 15\n" +
+//              "18 19 22 23\n" +
+//              "26 27 30 31";
+//
+//        String set3 = " 4 5 6 7\n" +
+//             "12 13 14 15\n" +
+//              "20 21 22 23\n" +
+//              "28 29 30 31";
+//
+//        String set4 = " 8 9 10 11\n" +
+//              "12 13 14 15\n" +
+//             "24 25 26 27\n" +
+//              "28 29 30 31";
+//
+//        String set5 = "16 17 18 19\n" +
+//              "20 21 22 23\n" +
+//              "24 25 26 27\n" +
+//              "28 29 30 31";
+//        int day = 0;
+//        Scanner input = new Scanner(System.in);
+//        System.out.print(set1);
+//        System.out.print("\nEnter Yes or No  ");
+//        String answer = input.nextLine();
+//        char ch1 = answer.charAt(0);
+//        if (ch1 == 'y'){
+//        day +=1;}
+//        System.out.print("\nIs your birthday in Set2?\n");
+//        System.out.print(set2);
+//        System.out.print("\nEnter 0 for No and 1 for Yes: ");
+//        answer = input.nextLine();
+//        char ch2 = answer.charAt(0);
+//        if (ch2 == 'y'){
+//        day +=2;}
+//        System.out.print("\nIs your birthday in Set3?\n");
+//        System.out.print(set3);
+//        System.out.print("\nEnter 0 for No and 1 for Yes: ");
+//        answer = input.nextLine();
+//        char ch3 = answer.charAt(0);
+//        if (ch3 == 'y'){
+//        day +=4;}
+//        System.out.print("\nIs your birthday in Set4?\n");
+//        System.out.print(set4);
+//        System.out.print("\nEnter 0 for No and 1 for Yes: ");
+//        answer = input.nextLine();
+//        char ch4 = answer.charAt(0);
+//        if (ch4 == 'y'){
+//        day +=8;}
+//        System.out.print("\nIs your birthday in Set5?\n");
+//        System.out.print(set5);
+//        System.out.print("\nEnter 0 for No and 1 for Yes: ");
+//        answer = input.nextLine();
+//        char ch5 = answer.charAt(0);
+//        if (ch5 == 'y'){
+//        day +=16;}
+//        System.out.println("\nYour birthday is " + day + "!");
 
-        String set2 = " 2 3 6 7\n" +
-             "10 11 14 15\n" +
-              "18 19 22 23\n" +
-              "26 27 30 31";
 
-        String set3 = " 4 5 6 7\n" +
-             "12 13 14 15\n" +
-              "20 21 22 23\n" +
-              "28 29 30 31";
-
-        String set4 = " 8 9 10 11\n" +
-              "12 13 14 15\n" +
-             "24 25 26 27\n" +
-              "28 29 30 31";
-
-        String set5 = "16 17 18 19\n" +
-              "20 21 22 23\n" +
-              "24 25 26 27\n" +
-              "28 29 30 31";
-        int day = 0;
         Scanner input = new Scanner(System.in);
-        System.out.print(set1);
-        System.out.print("\nEnter Yes or No  ");
-        String answer = input.nextLine();
-        char ch1 = answer.charAt(0);
-        if (ch1 == 'y'){
-        day +=1;}
-        System.out.print("\nIs your birthday in Set2?\n");
-        System.out.print(set2);
-        System.out.print("\nEnter 0 for No and 1 for Yes: ");
-        answer = input.nextLine();
-        char ch2 = answer.charAt(0);
-        if (ch2 == 'y'){
-        day +=2;}
-        System.out.print("\nIs your birthday in Set3?\n");
-        System.out.print(set3);
-        System.out.print("\nEnter 0 for No and 1 for Yes: ");
-        answer = input.nextLine();
-        char ch3 = answer.charAt(0);
-        if (ch3 == 'y'){
-        day +=4;}
-        System.out.print("\nIs your birthday in Set4?\n");
-        System.out.print(set4);
-        System.out.print("\nEnter 0 for No and 1 for Yes: ");
-        answer = input.nextLine();
-        char ch4 = answer.charAt(0);
-        if (ch4 == 'y'){
-        day +=8;}
-        System.out.print("\nIs your birthday in Set5?\n");
-        System.out.print(set5);
-        System.out.print("\nEnter 0 for No and 1 for Yes: ");
-        answer = input.nextLine();
-        char ch5 = answer.charAt(0);
-        if (ch5 == 'y'){
-        day +=16;}
-        System.out.println("\nYour birthday is " + day + "!");
+        System.out.println("please enter the integer");
+        int i = input.nextInt();
+        if (i > 15){
+            System.out.println(" invalid number ");
+        }
+        if ( i < 10){
+            System.out.println(i);
+        }
+        char c;
+        switch (i){
+            case 10: c = 'A'; System.out.println(c); break;
+            case 11: c = 'B'; System.out.println(c); break;
+            case 12: c = 'C'; System.out.println(c); break;
+            case 13: c = 'D'; System.out.println(c); break;
+            case 14: c = 'E'; System.out.println(c); break;
+            case 15: c = 'F'; System.out.println(c); break;
+
+        }
+
+
+
+
+
 
     }
 }
